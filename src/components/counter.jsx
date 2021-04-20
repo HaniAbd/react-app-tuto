b8ef0c8ba759cf3bb36e4ce9580a29eaea4f22f6
Hani <hani.abdulwahab@datategy.net>
2021-04-20 22:57:24 +0200

    add bootstrap create button and add static bootstrap class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,20 @@
 import React, {Component} from "react";
 
 class Counter extends Component {
+    state = {
+        count : 0,
+    };
     render() {
         return (
             <div>
-                <h1>Hello world</h1>
-                <button>Increment</button>
+                <span className="badge badge-dark m-2">{this.state.count}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
+    }
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 
