9b6cdd6ee5828784e19661ee5622717fe6c314b0
Hani <hani.abdulwahab@datategy.net>
2021-04-21 21:33:44 +0200

    render list of items

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, {Component} from "react";
 class Counter extends Component {
     state = {
         count : 1,
+        tags : ['tag1', 'tag2', 'tag3']
     };
     styles = {
         fontWeight : 'bold',
@@ -18,6 +19,12 @@ class Counter extends Component {
 
                 <span style={this.styles} className={this.getBadgeColor()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+
+                <div>
+                    <ul>
+                        {this.state.tags.map(tag => <li key={tag}> {tag} </li>)}
+                    </ul>
+                </div>
             </div>
         );
     }
