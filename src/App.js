a77258a852cc4689d03fb8c08102bd44ecfae6d1
Hani <hani.abdulwahab@datategy.net>
2021-04-26 21:00:10 +0200

    apply navBar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import NavBar from "./components/navBar";
+import Counters from "./components/counters";
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar/>
+                <main className='container'>
+                    <Counters></Counters>
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
