1f8e3a6041dfec4081f839c1bfc30db2691d8da0
Hani <hani.abdulwahab@datategy.net>
2021-04-26 12:08:43 +0200

    handle delete event and update state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, {Component} from "react";
 
 class Counter extends Component {
     state = {
-        value : this.props.value
+        value : this.props.counter.value
     };
 
     handleIncrement = () => {
         // this.state.count++; this not work, because react not aware of this change so we have to use setState
-        this.setState({count: this.state.value + 1});
+        this.setState({value: this.state.value + 1});
     };
 
 
@@ -17,8 +17,13 @@ class Counter extends Component {
                 {this.props.children}
                 <span style={this.styles} className={this.getBadgeColor()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
+    }
+
+    handleDelete() {
+
     }
 
     formatCount() {
